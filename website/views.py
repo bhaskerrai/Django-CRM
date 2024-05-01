fe32d38846fd86e301fb6e61b892ae39519d626d
bhaskerrai <bhaskerrai.dev@gmail.com>
2024-05-01 20:06:38 +0500

    added user authorization

--- website/views.py
+++ website/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
+def home(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            messages.success(request, "Login Successful")
+            return redirect("home")
+    
+        else:
+            messages.error(request, "Login Failed! Please try again")
+            return redirect("home")
+
+    else:
+        return render(request, "home.html", {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have successfully logged out!")
+    return redirect("home")
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(request, username=username, password=password)
+
+            login(request, user)
+            messages.success(request, "You have successfully registered!")
+            return redirect("home")
+
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {"form": form})
+        
+    return render(request, "register.html", {"form": form})
